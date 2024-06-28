49eea443580a40f3c71f9797975329c340fe1e5c
405708 <405708@tecnicatura.frc.utn.edu.ar>
2024-06-28 04:04:17 -0300

    Actualizacion

---
+++ Proyectos Spring io/more/p2-2w2/src/test/java/ar/edu/utn/frc/tup/lc/iii/services/impl/GameServiceImplTest.java
@@ -0,0 +1,21 @@
+package ar.edu.utn.frc.tup.lc.iii.services.impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class GameServiceImplTest {
+
+
+    @Test
+    void setGameResultByGameIdTest() {
+        // TODO Testing
+    }
+
+    @Test
+    void predictTest() {
+        // TODO Testing
+    }
+}
